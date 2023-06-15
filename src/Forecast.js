6103bb5ef9b1091d71007b7839d269558f69012c
RocioMartinez <rym5411@gmail.com>
2023-06-15 13:31:02 -0400

    added movie feature

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import Table from 'react-bootstrap/Table';
 
 
 
 class Forecast extends React.Component {
   render() {
-    return(
+    return (
       <>
-       <h2>Weather Forecast</h2>
-       {this.props.forecastInfo.map((day,index) => 
-       {return <div key={index} className='weather'>
-        <p>Date: {day.date}</p>
-        <p>Weather: {day.description}</p>
-        
-        </div>})};
+        <h2>Weather Forecast</h2>
+
+        {this.props.forecastInfo.map((day, index) => {
+          return <Table striped bordered hover variant="dark" key={index} className='weather'>
+            <thead>
+              <tr>
+                <th>Date: </th>
+                <th>Weather:</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{day.date}</td>
+                <td>{day.description}</td>
+              </tr>
+            </tbody>
+          </Table>
+        })};
       </>
     )
   }
